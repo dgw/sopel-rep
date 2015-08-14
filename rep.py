60a7b1ef81d91458d640c6ae17a273f1b5caf4b5
dgw <dgw@technobabbl.es>
2015-08-14 08:21:27 -0700

    Add/commit .luv command

--- rep.py
+++ rep.py
@@ -6,6 +6,20 @@ Copyright 2015 dgw
 from willie import module
 
 
+@module.commands('luv')
+@module.rate(3600)
+@module.example(".luv johnnytwothumbs")
+def luv(bot, trigger):
+    if not trigger.group(3):
+        bot.reply("No user specified.")
+        return
+    target = trigger.group(3)
+    rep = bot.db.get_nick_value(target, 'rep_score') or 0
+    rep += 1
+    bot.db.set_nick_value(target, 'rep_score', rep)
+    bot.say("%s has increased %s's reputation score to %d." % (trigger.nick, target, rep))
+
+
 @module.commands('rep')
 @module.example(".rep johnnytwothumbs")
 def rep(bot, trigger):
