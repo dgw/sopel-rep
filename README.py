7886ed0407bc62053d1786b7303eb8859daf1999
dgw <dgw@technobabbl.es>
2015-12-19 23:59:00 -0600

    add README

---
+++ README.py
@@ -0,0 +1,12 @@
+# willie-rep
+Lets users "luv" and "h8" other users on IRC. Clone of a mIRC script someone
+used in a channel I was in.
+
+## Dependencies
+Aside from willie and its dependencies, this module has no dependencies.
+(`time` is part of the standard Python library.)
+
+## Commands
+* `.luv nick`: Adds +1 to the user's reputation score
+* `.h8 nick`: Adds -1 to the user's reputation score
+
