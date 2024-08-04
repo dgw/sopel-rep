9ef4fd10936c409dceaf4c273804d5da05986f2f
dgw <dgw@technobabbl.es>
2024-08-04 03:25:11 -0700

    Refactor a bunch of helpers into a manager class + Sopel 8 only
    Went Sopel 8+ only because of the new `bot.make_identifier()` helper
    method, and the stronger checks around `SopelIdentifierMemory` keys
    (used for things like channels/users), and the better enforcement in
    `bot.db` of making sure the nick passed to `*_nick_value` methods is
    correctly treated as an `Identifier`.
    
    Basically, Sopel 7 is still a bit of a slopfest when it comes to dealing
    with `Identifier` values correctly when compared to 8. I wanted to get
    rid of as many typecasts in the plugin code as possible.

--- sopel_rep/plugin.py
+++ sopel_rep/plugin.py
@@ -7,14 +7,13 @@ Copyright 2015-2024 dgw
 from __future__ import annotations
 
 import re
-import time
 
 from sopel import plugin
 from sopel.config.types import BooleanAttribute, StaticSection, ValidatedAttribute
-from sopel.tools import Identifier, time as time_tools
+from sopel.tools import time as time_tools
 
-
-r_nick = r'[a-zA-Z0-9\[\]\\`_\^\{\|\}-]{1,32}'
+from .errors import ArgumentError, CooldownError, NonexistentNickError
+from .manager import RepManager, r_nick
 
 
 class RepSection(StaticSection):
@@ -23,7 +22,10 @@ class RepSection(StaticSection):
 
 
 def setup(bot):
+    global MANAGER
+
     bot.config.define_section('rep', RepSection)
+    MANAGER = RepManager(bot)
 
 
 def configure(config):
@@ -64,47 +66,29 @@ def luv_h8_cmd(bot, trigger):
     if not trigger.group(3):
         bot.reply("No user specified.")
         return
-    target = Identifier(trigger.group(3))
-    luv_h8(bot, trigger, target, trigger.group(1))
+
+    luv_h8(bot, trigger, trigger.group(3), trigger.group(1))
 
 
 def luv_h8(bot, trigger, target, which, warn_nonexistent=True):
-    target = verified_nick(bot, target, trigger.sender)
-    which = which.lower()  # issue #18
-    pfx = change = selfreply = None  # keep PyCharm & other linters happy
-    if not target:
+    try:
+        message = MANAGER.luv_or_h8(trigger, target, which)
+    except NonexistentNickError as err:
         if warn_nonexistent:
-            command = which
-            try:  # because a simple "trigger.group('command') or which" doesn't work
-                command = trigger.group('command')
-            except IndexError:  # why can't you just return None, re.group()?
-                pass
-            bot.reply("You can only %s someone who is here." % command)
+            bot.reply(str(err))
         return False
-    if (
-        not (trigger.admin and not bot.settings.rep.admin_cooldown)
-        and rep_too_soon(bot, trigger.nick)
-    ):
+    except ArgumentError as err:
+        bot.reply(str(err))
         return False
-    if which == 'luv':
-        selfreply = "No narcissism allowed!"
-        pfx, change = 'in', 1
-    if which == 'h8':
-        selfreply = "Go to 4chan if you really hate yourself!"
-        pfx, change = 'de', -1
-    if not (pfx and change and selfreply):  # safeguard against leaving something in the above mass-None assignment
-        bot.say("Logic error! Please report this to %s." % bot.config.core.owner)
-        return
-    if is_self(bot, trigger.nick, target):
-        bot.reply(selfreply)
+    except CooldownError as err:
+        bot.notice(
+            "You can change someone's rep again {}."
+            .format(time_tools.seconds_to_human(-err.remaining_time)),
+            trigger.nick,
+        )
         return False
 
-    possessive = 'my' if target == bot.nick else target + "'s"
-    if bot.db.get_nick_value(target, 'rep_locked'):
-        bot.reply("Sorry, %s reputation has been locked by an admin." % possessive)
-        return False
-    rep = mod_rep(bot, trigger.nick, target, change)
-    bot.say("%s has %screased %s reputation score to %d" % (trigger.nick, pfx, possessive, rep))
+    bot.say(message)
     return True
 
 
@@ -112,7 +96,7 @@ def luv_h8(bot, trigger, target, which, warn_nonexistent=True):
 @plugin.example(".rep johnnytwothumbs")
 def show_rep(bot, trigger):
     target = trigger.group(3) or trigger.nick
-    rep = get_rep(bot, target)
+    rep = MANAGER.get_rep(target)
     if rep is None:
         bot.say("%s has no reputation score yet." % target)
         return
@@ -127,80 +111,9 @@ def manage_locks(bot, trigger):
     if not target:
         bot.reply("I need a nickname!")
         return
-    if 'un' in trigger.group(1):  # .repunlock command used
-        bot.db.set_nick_value(Identifier(target), 'rep_locked', False)
+    if 'unlock' in trigger.group(1):  # .repunlock command used
+        MANAGER.unlock_rep(target)
         bot.say("Unlocked rep for %s." % target)
     else:  # .replock command used
-        bot.db.set_nick_value(Identifier(target), 'rep_locked', True)
+        MANAGER.lock_rep(target)
         bot.say("Locked rep for %s." % target)
-
-
-# helpers
-def get_rep(bot, target):
-    return bot.db.get_nick_value(Identifier(target), 'rep_score')
-
-
-def set_rep(bot, caller, target, newrep):
-    bot.db.set_nick_value(Identifier(target), 'rep_score', newrep)
-    bot.db.set_nick_value(Identifier(caller), 'rep_used', time.time())
-
-
-def mod_rep(bot, caller, target, change):
-    rep = get_rep(bot, target) or 0
-    rep += change
-    set_rep(bot, caller, target, rep)
-    return rep
-
-
-def get_rep_used(bot, nick):
-    return bot.db.get_nick_value(Identifier(nick), 'rep_used') or 0
-
-
-def set_rep_used(bot, nick):
-    bot.db.set_nick_value(Identifier(nick), 'rep_used', time.time())
-
-
-def rep_used_since(bot, nick):
-    now = time.time()
-    last = get_rep_used(bot, nick)
-    return abs(last - now)
-
-
-def rep_too_soon(bot, nick):
-    since = rep_used_since(bot, nick)
-    cooldown = bot.settings.rep.cooldown
-    if since < cooldown:
-        bot.notice("You can change someone's rep again %s." % time_tools.seconds_to_human(-(cooldown - since)), nick)
-        return True
-    else:
-        return False
-
-
-def is_self(bot, nick, target):
-    nick = Identifier(nick)
-    target = Identifier(target)
-    if nick == target:
-        return True  # shortcut to catch common goofballs
-    try:
-        nick_id = bot.db.get_nick_id(nick, False)
-        target_id = bot.db.get_nick_id(target, False)
-    except ValueError:
-        return False  # if either nick doesn't have an ID, they can't be in a group
-    return nick_id == target_id
-
-
-def verified_nick(bot, nick, channel):
-    if not all([nick, channel]):
-        # `bot` is always going to be a thing, but `nick` or `channel` could be empty
-        # and that means verification should immediately fail
-        return ''  # not None; see below
-
-    nick = re.search(r_nick, nick).group(0)
-    if not nick:
-        return ''  # returning None would mean the returned value can't be compared with ==
-    if nick not in bot.channels[channel].users:
-        if nick.endswith('--'):
-            if nick[:-2] in bot.channels[channel].users:
-                return Identifier(nick[:-2])
-        return ''  # see above
-    return Identifier(nick)
